8f84cde7b92d0747d283341b1bd081ca4f3dde5c
vanshverma721 <vvv.721781@gmail.com>
2024-10-03 18:07:11 +0530

    added code to display data of sidebar

--- src/components/SideBarContent.jsx
+++ src/components/SideBarContent.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Box, Button, styled } from '@mui/material'
+import { Box, Button, styled, List, ListItem } from '@mui/material'
 import { CreateOutlined } from '@mui/icons-material'
-
+import { SIDEBAR_DATA } from '../config/sidebar.config'
 const ComposeButton = styled(Button)({
     background: '#c2e7ff',
     color: '#001d35',
@@ -17,7 +17,16 @@ const SideBarContent = () => {
             <ComposeButton>
                 <CreateOutlined />Compose
             </ComposeButton>
-            <Box></Box>
+            <List>
+                {
+                    SIDEBAR_DATA.map(data => (
+                        <ListItem>
+                            <data.icon fontSize='small'/>{data.title}
+                        </ListItem>
+                    ))
+                }
+            </List>
+
         </Box>
     )
 }
