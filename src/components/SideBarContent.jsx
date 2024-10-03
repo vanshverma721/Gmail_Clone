44c5d7a3922e416479de292d3f96980d99d7b56b
vanshverma721 <vvv.721781@gmail.com>
2024-10-03 18:15:06 +0530

    added some styling for sidebar

--- src/components/SideBarContent.jsx
+++ src/components/SideBarContent.jsx
@@ -2,18 +2,35 @@ import React from 'react'
 import { Box, Button, styled, List, ListItem } from '@mui/material'
 import { CreateOutlined } from '@mui/icons-material'
 import { SIDEBAR_DATA } from '../config/sidebar.config'
+
 const ComposeButton = styled(Button)({
     background: '#c2e7ff',
     color: '#001d35',
     padding: 15,
     borderRadius: 16,
     minWidth: 140,
-    textTransform: 'none'
+    textTransform: 'none',
+    display: 'flex',
+    justifyContent: 'space-around'
+})
+
+const Container = styled(Box)({
+    padding: 8,
+    '& > ul': {
+        padding: '10px 0 0 5px',
+        fontSize: 14,
+        fontWeigth: 500,
+        cursor: 'Pointer'
+    },
+    '& > ul > li > svg': {
+
+        marginRight: 20
+    }
 })
 
 const SideBarContent = () => {
     return (
-        <Box>
+        <Container>
             <ComposeButton>
                 <CreateOutlined />Compose
             </ComposeButton>
@@ -27,7 +44,7 @@ const SideBarContent = () => {
                 }
             </List>
 
-        </Box>
+        </Container>
     )
 }
 
