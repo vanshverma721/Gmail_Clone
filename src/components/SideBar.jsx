c461ca206c31a09f1fc5904adc8da666c06a5331
vanshverma721 <vvv.721781@gmail.com>
2024-10-03 17:42:20 +0530

    made side bar structure

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { Drawer, styled } from '@mui/material'
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <Drawer
+        anchor='left'
+        open={true}
+        hideBackdrop={true}
+        ModalProps={{
+            keepMounted: true
+        }}
+        variant='persistant'
+        sx={{
+            '& .MuiDrawer-paper': {
+                marginTop: '64px',
+                width: 250,
+                background: '#F5F5F5',
+                borderRight: 'none',
+                height: 'calc(100vh - 64px)'
+            }
+        }}
+    >
+         
+    </Drawer>
   )
 }
 
