549e68e4f34067ac28d9264a3f110a9964b69186
vanshverma721 <vvv.721781@gmail.com>
2024-10-03 17:44:42 +0530

    passing content in sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import { Drawer, styled } from '@mui/material'
+import SideBarContent from './SideBarContent'
 
 const SideBar = () => {
-  return (
-    <Drawer
-        anchor='left'
-        open={true}
-        hideBackdrop={true}
-        ModalProps={{
-            keepMounted: true
-        }}
-        variant='persistant'
-        sx={{
-            '& .MuiDrawer-paper': {
-                marginTop: '64px',
-                width: 250,
-                background: '#F5F5F5',
-                borderRight: 'none',
-                height: 'calc(100vh - 64px)'
-            }
-        }}
-    >
-         
-    </Drawer>
-  )
+    return (
+        <Drawer
+            anchor='left'
+            open={true}
+            hideBackdrop={true}
+            ModalProps={{
+                keepMounted: true
+            }}
+            variant='persistant'
+            sx={{
+                '& .MuiDrawer-paper': {
+                    marginTop: '64px',
+                    width: 250,
+                    background: '#F5F5F5',
+                    borderRight: 'none',
+                    height: 'calc(100vh - 64px)'
+                }
+            }}
+        >
+            <SideBarContent />
+        </Drawer>
+    )
 }
 
 export default SideBar
---
+++ src/components/SideBarContent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SideBarContent = () => {
+  return (
+    <div>
+        Inbox
+    </div>
+  )
+}
+
+export default SideBarContent
