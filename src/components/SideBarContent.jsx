8cff920cfa7089b637b9c4c2f8890a95a6739838
vanshverma721 <vvv.721781@gmail.com>
2024-10-03 17:56:16 +0530

    added compose button

--- src/components/SideBarContent.jsx
+++ src/components/SideBarContent.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { Box, Button, styled } from '@mui/material'
+import { CreateOutlined } from '@mui/icons-material'
+
+const ComposeButton = styled(Button)({
+    background: '#c2e7ff',
+    color: '#001d35',
+    padding: 15,
+    borderRadius: 16,
+    minWidth: 140,
+    textTransform: 'none'
+})
 
 const SideBarContent = () => {
-  return (
-    <div>
-        Inbox
-    </div>
-  )
+    return (
+        <Box>
+            <ComposeButton>
+                <CreateOutlined />Compose
+            </ComposeButton>
+            <Box></Box>
+        </Box>
+    )
 }
 
 export default SideBarContent
