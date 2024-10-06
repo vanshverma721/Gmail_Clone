cc79a8b60b549bf01e3844051f1f6aa99cdbf94d
vanshverma721 <vvv.721781@gmail.com>
2024-10-06 22:15:27 +0530

    Making modal for compose mail

---
+++ src/components/ComposeMail.jsx
@@ -0,0 +1,23 @@
+import { Dialog } from "@mui/material"
+
+const dialogStyle = {
+    height: '90%',
+    width: '80%',
+    maxWidth: '100%',
+    maxHeight: '100%',
+    boxShadow: 'none',
+    borderRadius: '10px 10px 0 0'
+}
+
+const ComposeMail = () => {
+    return (
+        <Dialog
+            open={true}
+            PaperProps={{ sx: dialogStyle }}
+        >
+            Hello
+        </Dialog>
+    )
+}
+
+export default ComposeMail
